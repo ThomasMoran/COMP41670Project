a736c99a1c741c951a601feed090b609405ecffb
Thomas Moran <thomaslmoran.14@gmail.com>
2015-09-29 15:50:04 +0100

    New GUIInterface

---
+++ COMP41670Project/src/COMP41670/COMP41670Project/Package/GUIInterface.java
@@ -0,0 +1,14 @@
+package COMP41670.COMP41670Project.Package;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class GUIInterface extends JFrame {
+
+	public GUIInterface() {
+	
+		this.setBackground(Color.black);
+	}
+	
+}
--- COMP41670Project/src/COMP41670/COMP41670Project/Package/Main.java
+++ COMP41670Project/src/COMP41670/COMP41670Project/Package/Main.java
@@ -1,10 +1,14 @@
 package COMP41670.COMP41670Project.Package;
 
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame {
+	public Main() {
+	}
 
 	public static void main(String[] args) {
 		// Test Comment
-		System.out.println("Hello World");
+		
 		System.out.println("Hello World");
 
 	}
